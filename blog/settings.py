6e77ce4cdbd37005c6d93199fafcc05ccd39e604
Afeez lawal <45849676+AfeezGL@users.noreply.github.com>
2020-09-20 12:19:42 +0100

    Update settings.py

--- blog/settings.py
+++ blog/settings.py
@@ -38,11 +38,10 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'storages',
     'ckeditor',
     'ckeditor_uploader',
     'hitcount',
-    'django.contrib.staticfiles',
-    'storages',
 ]
 
 MIDDLEWARE = [
