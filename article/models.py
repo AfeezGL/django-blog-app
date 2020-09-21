8e72d9779a7e7658ca4a6687da7c2ef2547cf3b2
Afeez lawal <45849676+AfeezGL@users.noreply.github.com>
2020-09-21 01:36:48 +0100

    Add files via upload

---
+++ article/models.py
@@ -0,0 +1,16 @@
+from django.db import models
+from autoslug import AutoSlugField
+
+class Article(models.Model):
+    image = models.ImageField()
+    title = models.CharField(max_length = 350)
+    body = models.TextField()
+    pub_date = models.DateTimeField(auto_now = True)
+    update_date = models.DateTimeField(auto_now_add = True)
+    slug = AutoSlugField(populate_from = "title")
+    @property
+    def short_text(self):
+        return self.body[0:150]
+        
+    def __str__(self):
+        return self.title
