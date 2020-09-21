d9d3a45c41182b27d77d523e4d99524a4e0bceb3
Afeez lawal <45849676+AfeezGL@users.noreply.github.com>
2020-09-21 01:35:15 +0100

    Delete urls.py

--- article/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-
-from . import views
-
-urlpatterns = [
-    path('', views.IndexView.as_view(), name = 'index'),
-    path('<str:slug>/', views.DetailView.as_view(), name = 'details'),
-
-]
